b3ea86a53de77140dda56e52e094ce8557140613
isaias0rt0n <62946999+isaias0rt0n@users.noreply.github.com>
2021-01-13 19:43:36 -0300

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,20 +1,20 @@
 import requests, hashlib
 from bs4 import BeautifulSoup
 
-URL = "http://142.93.34.50:32402/"
+URL = "http://site.exemplo:p0rt4/" #URL do desafio
 
 session = requests.Session()
 
 def hash_md5(hash_value):
     return hashlib.md5(hash_value.encode()).hexdigest()
 
-response = session.get(URL)
+response = session.get(URL) #faz a requisição
 
-soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(response.text, 'html.parser')  # pega o code html
 
-md5 = hash_md5(soup.h3.string)
+md5 = hash_md5(soup.h3.string)  # pega a info onde está a string requerida
 
-flag = session.post(URL, data={'hash':md5})
+flag = session.post(URL, data={'hash':md5}) # faz o post do problema
 
 soup_flag = BeautifulSoup(flag.text, 'html.parser')
-print("Flag: {}".format(soup_flag.p.string))
+print("Flag: {}".format(soup_flag.p.string))    #imprime a flag encontrada
